621ec07379bb4ce72a73003f0e06380fd19b8f39
xushuailong <xuafeiJelly123@outlook.com>
2018-12-19 16:32:43 +0800

    计算机逻辑

--- pages/calculator/calc.js
+++ pages/calculator/calc.js
@@ -1,4 +1,32 @@
 // pages/calculator/calc.js
+
+
+var calculate = function(data1, oper, data2) {
+  var data;
+  data1 = parseFloat(data1);
+  data2 = parseFloat(data2);
+  switch (oper) {
+    case "+":
+      data = data1 + data2;
+      break;
+    case "-":
+      data = data1 - data2;
+      break;
+    case "*":
+      data = data1 * data2;
+      break;
+    case "/":
+      if (data2 !== 0) {
+        data = data1 / data2;
+      } else {
+        data = 0;
+      }
+      break;
+  }
+  return data;
+};
+
+
 Page({
 
   /**
@@ -33,11 +61,78 @@ Page({
     id17: "negative", //取负
     id18: "num_0",
     id19: "dot", //小数点
-    id20: "equ" //等号
+    id20: "equ", //等号
+
+    temp: "0", //上次的临时结果
+    lastoper: "+", //上一次的运算符
+    flag: false //上一次非数字按钮标志
   },
 
   clickButton: function(e) { //单击事件处理函数
+    var data = this.data.result; // 获取上一个结果值
 
+    var temp = this.data.temp; // 取上次的临时结果
+    var lastoper1 = this.data.lastoper; //上一次的运算符
+    var noNumFlag = this.data.flag; //上一次非数字按钮标志
+
+    if (e.target.id >= 'num_0' && e.target.id <= 'num_9') { //判断是否按了数字按钮
+      data += e.target.id.split("_")[1]; //正常情况串接输入的值
+      if (this.data.result == "0" || noNumFlag) { //原值为0 或 上次按为非负数
+        data = e.target.id.split("_")[1]; //用输入的值代替
+      }
+      noNumFlag = false;
+    } else { //不是数字按钮
+      noNumFlag = true;
+      console.log(e.target.id); //在控制台输出按钮的id
+      if (e.target.id == "dot") { // 小数点
+        noNumFlag = false;
+        if (data.toString().indexOf(".") == -1) { //输入的值不包括小数点
+          data += ".";
+        }
+      } else if (e.target.id == "clear") { //清除按钮
+        data = 0; //数据清0
+        temp = 0; //清楚中间结果
+        lastoper1 = "+"; //设置上次运算符号为加
+      } else if (e.target.id == "negative") { //数字取负
+        noNumFlag = false;
+        data = -1 * data;
+      } else if (e.target.id == "back") { //回退一个字符
+        noNumFlag = false;
+        if (data.toString().length > 1) { //长度超过一位数
+          data = data.substr(0, data.toString().length - 1); //去掉最后一位
+        } else { //长度只有一位
+          data = 0; //置0
+        }
+
+      } else if (e.target.id == "div") { //除法
+        data = calculate(temp, lastoper1, data);
+        temp = data;
+        lastoper1 = "/";
+      } else if (e.target.id == "mul") { //乘法
+        data = calculate(temp, lastoper1, data);
+        temp = data;
+        lastoper1 = "*";
+      } else if (e.target.id == "add") { //加法
+        data = calculate(temp, lastoper1, data);
+        temp = data;
+        lastoper1 = "+";
+      } else if (e.target.id == "sub") { //减法
+        data = calculate(temp, lastoper1, data);
+        temp = data;
+        lastoper1 = "-";
+      } else if (e.target.id == "equ") { //等号
+        data = calculate(temp, lastoper1, data);
+        temp = 0;
+        lastoper1 = "+";
+      }
+
+    }
+    this.setData({
+      result: data,
+      lastoper: lastoper1,
+      temp: temp,
+      flag: noNumFlag
+    })
   },
 
   bindAdd: function(e) {
